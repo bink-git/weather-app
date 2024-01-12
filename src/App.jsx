12e872af3ea90b56ad227af949090320bccec32d
BINK <bink@email.ua>
2024-01-12 15:44:39 +0200

    add main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState(null);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch(
+  //       `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6d0c694d48573d4cf670244cd6870ab3&units=metric`
+  //     );
+  //     const data = await response.json();
+  //     console.log(data);
+  //     setWeather(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          const apiKey = '6d0c694d48573d4cf670244cd6870ab3';
+          const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+          fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              setWeather(data);
+            })
+            .catch((error) => {
+              setError('Error fetching weather data');
+              console.error('Error fetching weather data:', error);
+            });
+        },
+        function (error) {
+          setError('Error getting location');
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      setError('Geolocation is not supported by this browser.');
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {weather && (
+          <>
+            <h1>{weather.name}</h1>
+            <p>Temperature: {Math.round(weather.main.temp)}°C</p>
+            <p>Humidity: {Math.round(weather.main.humidity)}%</p>
+          </>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
