882ae7106ecf5c1f5b83f36b44c76d1b5063b50b
BINK <bink@email.ua>
2024-01-13 13:40:02 +0200

    add message when city not found

--- src/App.jsx
+++ src/App.jsx
@@ -40,36 +40,38 @@ function App() {
 
   const searchCity = () => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    // setError('');
+
     fetch(apiUrl)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('City not found');
+        }
+        return response.json();
+      })
       .then((data) => {
         setWeather(data);
+        setError('');
       })
       .catch((error) => {
-        setError('Error fetching weather data');
         console.error('Error fetching weather data:', error);
+        if (error.message === 'City not found') {
+          setError('City not found. Please enter a valid city name.');
+        } else {
+          setError('Error fetching weather data');
+        }
+      })
+      .finally(() => {
+        setCity('');
       });
-    setCity('');
   };
 
   return (
     <>
       <div className="card">
-        {/* {error && <p>{error}</p>} */}
-        {error && (
-          <form onSubmit={(e) => e.preventDefault()}>
-            <p htmlFor="location" style={{ fontSize: '1.8rem' }}>
-              Please, enter your city
-            </p>
-            <input
-              id="location"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <button onClick={searchCity}>Search</button>
-          </form>
-        )}
-        {weather && (
+        {error && <p>{error}</p>}
+
+        {weather ? (
           <>
             <h1>
               {weather.name}, {weather.sys.country}
@@ -84,6 +86,18 @@ function App() {
               />
             </div>
           </>
+        ) : (
+          <form onSubmit={(e) => e.preventDefault()}>
+            <p htmlFor="location" style={{ fontSize: '1.8rem' }}>
+              Please, enter your city
+            </p>
+            <input
+              id="location"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button onClick={searchCity}>Search</button>
+          </form>
         )}
       </div>
     </>
